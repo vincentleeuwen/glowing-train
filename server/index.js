1e78753af35bc15609cb634f552cdf0f0a45ebe3
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-12-12 16:48:13 +0100

    initial server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup
+// Middleware that any request will go through
+app.use(morgan('combined')); // logging framework
+app.use(bodyParser.json({ type: '*/*' })); // parse any request as json
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on', port);
