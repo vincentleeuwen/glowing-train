7b4f8e010b5ad9ae37d5e19d05d58b3096345ae3
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-12-13 17:38:49 +0100

    add signin route

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -7,6 +7,12 @@ const tokenForUser = user => {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = (req, res, next) => {
+  // User has already had their email and password auth'd.
+  // We just need to give them a token.
+  res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = (req, res, next) => {
   // Get email from request
   const { body: { email, password } } = req;
--- server/router.js
+++ server/router.js
@@ -3,8 +3,10 @@ const Authentication = require('./controllers/authentication');
 const passportService = require('./services/passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = (app) => {
-  app.get('/', requireAuth, (req, res) => ( res.send('you are logged in!') ));
+  app.get('/', requireAuth, (req, res) => ( res.send('you are logged in!')));
+  app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 }
